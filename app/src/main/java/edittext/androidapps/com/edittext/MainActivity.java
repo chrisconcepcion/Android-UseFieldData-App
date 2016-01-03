6550ccbeebad8e7a9bd83ea55a0fc2953e94257d
Christopher Concepcion <feverdoingwork@gmail.com>
2016-01-02 21:13:26 -0500

    first commit

---
+++ app/src/main/java/edittext/androidapps/com/edittext/MainActivity.java
@@ -0,0 +1,79 @@
+package edittext.androidapps.com.edittext;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // EditText variable declaration
+    private EditText myEditText;
+
+    private TextView myTextView;
+    private Button myButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // assign EditText variable
+        myEditText = (EditText)findViewById(R.id.editText);
+
+        myTextView = (TextView)findViewById(R.id.textViewId);
+        myButton = (Button)findViewById(R.id.buttonId);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            private String name;
+
+            @Override
+            public void onClick(View v) {
+                // set name variable from EditText field
+                name = myEditText.getText().toString();
+                myTextView.setText(name);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
